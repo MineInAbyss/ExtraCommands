7feeeb2c82f45a64fd0b379299b3af642723d79f
Boy <sivertpaulsen2@gmail.com>
2023-12-04 15:53:32 +0100

    make mia command urls clickable

--- src/main/kotlin/com/mineinabyss/extracommands/commands/MineInAbyssCommands.kt
+++ src/main/kotlin/com/mineinabyss/extracommands/commands/MineInAbyssCommands.kt
@@ -10,32 +10,32 @@ fun CommandDSLEntrypoint.mineInAbyssCommands() {
     command("orespawns") {
         playerAction {
             player.info("""
-                <#00bfff><b>You can find the different ore spawnrates here:
-                <#ffd500><underlined>https://mineinabyss.com/orespawns
+                <#00bfff><b>You can find the different ore spawnrates here:</b>
+                <click:open_url:'https://mineinabyss.com/orespawns'><#ffd500><underlined>https://mineinabyss.com/orespawns
                 """.trimIndent())
         }
     }
     command("mobdrops") {
         playerAction {
             player.info("""
-                <#37a600><b>All the mobdrops can be found here:
-                <#6ff030><underlined>https://mineinabyss.com/mobdrops
+                <#37a600><b>All the mobdrops can be found here:</b>
+                <click:open_url:'https://mineinabyss.com/mobdrops'><#6ff030><underlined>https://mineinabyss.com/mobdrops
             """.trimIndent())
         }
     }
     command("villagertrades") {
         playerAction {
             player.info("""
-                <#4c00fc><b>The Orth Trading prices can be found here:
-                <#008bfc><underlined>https://mineinabyss.com/villagertrades
+                <#4c00fc><b>The Orth Trading prices can be found here:</b>
+                <click:open_url:'https://mineinabyss.com/villagertrades'><#008bfc><underlined>https://mineinabyss.com/villagertrades
             """.trimIndent())
         }
     }
     command("website") {
         playerAction {
             player.info("""
-                <#4c00fc><b>The server's website can be found here:
-                <#008bfc><underlined>https://www.mineinabyss.com
+                <#4c00fc><b>The server's website can be found here:</b>
+                <click:open_url:'https://mineinabyss.com'><#008bfc><underlined>https://www.mineinabyss.com
             """.trimIndent())
         }
     }
