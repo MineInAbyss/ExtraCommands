75d9d19298e27413f625dec48ba08e19a72be549
Danielle Voznyy <dan.voznyy@gmail.com>
2024-01-14 23:58:08 -0500

    Revert version bump since prod isn't on them yet, bump our version to try fix publication error

--- src/main/kotlin/com/mineinabyss/extracommands/commands/ItemRenameCommand.kt
+++ src/main/kotlin/com/mineinabyss/extracommands/commands/ItemRenameCommand.kt
@@ -13,7 +13,6 @@ import com.mineinabyss.idofront.commands.extensions.actions.playerAction
 import com.mineinabyss.idofront.items.editItemMeta
 import com.mineinabyss.idofront.messaging.error
 import com.mineinabyss.idofront.serialization.SerializableItemStack
-import com.mineinabyss.idofront.serialization.SerializableItemStackProperties
 import com.mineinabyss.idofront.textcomponents.miniMsg
 import org.bukkit.entity.Player
 import kotlin.math.max
@@ -34,13 +33,13 @@ fun CommandDSLEntrypoint.itemRenameCommand() {
                         // If has a display name set, stop display name migrations
                         val existingIgnore = persistentDataContainer.decode<SetItemIgnoredProperties>()?.ignore ?: setOf()
                         persistentDataContainer.encode(
-                            SetItemIgnoredProperties(existingIgnore.plus(SerializableItemStackProperties.DISPLAY_NAME))
+                            SetItemIgnoredProperties(existingIgnore.plus(SerializableItemStack.Properties.DISPLAY_NAME))
                         )
                     }
                     else -> {
                         // If no display name, make sure we allow migrations again
                         val existingIgnore = persistentDataContainer.decode<SetItemIgnoredProperties>()?.ignore ?: return@editItemMeta
-                        val ignore = existingIgnore.minus(SerializableItemStackProperties.DISPLAY_NAME)
+                        val ignore = existingIgnore.minus(SerializableItemStack.Properties.DISPLAY_NAME)
                         if (ignore.isEmpty()) persistentDataContainer.remove<SetItemIgnoredProperties>()
                         else persistentDataContainer.encode(SetItemIgnoredProperties(ignore))
                     }
