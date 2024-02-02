ac777106316b002aee6e567e95fbb90d1f60789d
Boy <sivertpaulsen2@gmail.com>
2024-02-02 12:59:09 +0100

    refactor: use Duration#toComponent instead of calculating time manually

--- src/main/kotlin/com/mineinabyss/extracommands/commands/SeenCommand.kt
+++ src/main/kotlin/com/mineinabyss/extracommands/commands/SeenCommand.kt
@@ -5,11 +5,14 @@ import com.mineinabyss.idofront.commands.arguments.offlinePlayerArg
 import com.mineinabyss.idofront.commands.arguments.stringArg
 import com.mineinabyss.idofront.commands.entrypoint.CommandDSLEntrypoint
 import com.mineinabyss.idofront.messaging.error
+import com.mineinabyss.idofront.textcomponents.miniMsg
 import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
 import java.time.Instant
 import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 fun CommandDSLEntrypoint.seenCommand() {
     command("seen") {
@@ -19,7 +22,7 @@ fun CommandDSLEntrypoint.seenCommand() {
             if (offlinePlayer.isOnline) return@action sender.error("A player with the name ${offlinePlayer.name} is currently online.")
 
             val timeSince = calculateTime(dateDifference(Date(offlinePlayer.lastSeen)))
-            sender.sendMessage(offlinePlayer.name + " was last seen " + timeSince.toString() + " ago.")
+            sender.sendMessage(("<gold><i>" + offlinePlayer.name + "</i> was last seen " + "<yellow>" + timeSince + "</yellow> ago.").miniMsg())
         }
     }
 }
@@ -28,25 +31,13 @@ fun seenTabComplete(args: Array<out String>) =
     extraCommands.plugin.server.onlinePlayers.map { it.name }.filter { it.startsWith(args[0], true) }
         .takeIf { args.size == 1 } ?: emptyList()
 
+fun calculateTime(s: Long) = s.toDuration(DurationUnit.MILLISECONDS).toComponents { days, hours, minutes, seconds, _ ->
+    var timeSince = "$days days, $hours hours, $minutes minutes, $seconds seconds"
+    if (days == 0L) timeSince = timeSince.removePrefix("0 days, ")
+    if (hours == 0) timeSince = timeSince.removePrefix("0 hours, ")
+    if (minutes == 0) timeSince = timeSince.removePrefix("0 minutes, ")
 
-class TimeSince(val days: Int, val hours: Long, val minutes: Long, val seconds: Long) {
-    override fun toString(): String {
-        var timeSince = "$days days $hours hours $minutes minutes $seconds seconds"
-        if (this.days == 0) timeSince = timeSince.removePrefix("0 days ")
-        if (this.hours == 0L) timeSince = timeSince.removePrefix("0 hours ")
-        if (this.minutes == 0L) timeSince = timeSince.removePrefix("0 minutes ")
-
-        return timeSince
-    }
-}
-
-fun calculateTime(s: Long): TimeSince {
-    val days = TimeUnit.MILLISECONDS.toDays(s).toInt()
-    val hours = TimeUnit.MILLISECONDS.toHours(s) - TimeUnit.DAYS.toHours(days.toLong())
-    val minutes = TimeUnit.MILLISECONDS.toMinutes(s) - TimeUnit.HOURS.toMinutes(hours)
-    val seconds =
-        TimeUnit.MILLISECONDS.toSeconds(s) - TimeUnit.HOURS.toSeconds(hours) - TimeUnit.MINUTES.toSeconds(minutes)
-    return TimeSince(days, hours, minutes, seconds)
+    timeSince
 }
 
 private fun dateDifference(seen: Date, timeUnit: TimeUnit = TimeUnit.MILLISECONDS): Long {
