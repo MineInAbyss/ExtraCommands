d4aff26b81da6e296084ce014471b2f5d3d01f82
Boy <sivertpaulsen2@gmail.com>
2025-02-04 14:49:18 +0100

    fix: make itemrename support emojy-original-item-rename logic

--- src/main/kotlin/com/mineinabyss/extracommands/commands/ItemRenameCommand.kt
+++ src/main/kotlin/com/mineinabyss/extracommands/commands/ItemRenameCommand.kt
@@ -7,15 +7,22 @@ import com.mineinabyss.idofront.messaging.error
 import com.mineinabyss.idofront.textcomponents.miniMsg
 import com.mineinabyss.idofront.textcomponents.serialize
 import com.mojang.brigadier.arguments.StringArgumentType
+import org.bukkit.NamespacedKey
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
 
+val EMOJY_ORIGINAL_ITEM_RENAME_TEXT = NamespacedKey.fromString("emojy:original_item_rename")!!
 fun RootIdoCommands.itemRenameCommand() {
     "itemrename" {
         playerExecutes(StringArgumentType.greedyString().suggests {
             (context.source.executor as? Player)?.inventory?.itemInMainHand?.itemMeta?.displayName()?.serialize()?.let { suggestFiltering(it) }
         }.default { "" }) { renamed ->
-            player.inventory.itemInMainHand.takeIf { !it.isEmpty }?.editItemMeta {
-                displayName(renamed?.takeIf { it.isNotEmpty() }?.miniMsg())
+            player.inventory.itemInMainHand.takeUnless(ItemStack::isEmpty)?.editItemMeta {
+                if (renamed.isNullOrEmpty()) persistentDataContainer.remove(EMOJY_ORIGINAL_ITEM_RENAME_TEXT)
+                else persistentDataContainer.set(EMOJY_ORIGINAL_ITEM_RENAME_TEXT, PersistentDataType.STRING, renamed)
+
+                displayName(renamed.takeUnless { it.isNullOrEmpty() }?.miniMsg())
             } ?: sender.error("You must be holding an item to rename it!")
         }
     }
