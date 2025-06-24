d0d314cb24e4b8b72be495abba5eb21f9d59307b
Boy0000 <sivertpaulsen2@gmail.com>
2025-06-24 23:07:44 +0200

    refactor: cleanup viewdistance cmd

--- src/main/kotlin/com/mineinabyss/extracommands/commands/ViewDistanceCommand.kt
+++ src/main/kotlin/com/mineinabyss/extracommands/commands/ViewDistanceCommand.kt
@@ -21,15 +21,29 @@ fun RootIdoCommands.viewDistanceCommands() {
             IntegerArgumentType.integer(0, 32).named("viewDistance").default { 0 },
             DurationTypeArgument(1.seconds).named("duration").default { Duration.INFINITE }
         ) { players, viewDistance, duration, ->
-            extraCommands.plugin.launch {
+            if (viewDistance == 0 && duration == Duration.INFINITE) {
+                val viewDistances = players.groupBy { it.viewDistance }.entries.sortedBy { it.value.size }
+                if (viewDistances.size == 1) {
+                    sender.success("ViewDistances for all players: ${viewDistances.first().key}")
+                } else sender.success(buildString {
+                    append("ViewDistances for ${viewDistances.size} players:\n")
+                    viewDistances.forEach { (vd, players) ->
+                        if (players.size > 5) append("${players.take(5).joinToString { it.name }},...: $vd\n")
+                        else append("${players.joinToString { it.name }}: $vd\n")
+                    }
+                })
+            } else {
                 players.forEach { it.viewDistance = viewDistance.takeIf { it >= 2 } ?: it.world.viewDistance }
                 sender.success("Successfully set ${if (players.size == 1) "<dark_green>${players.first().name}</dark_green>" else players.size.toString() + "players"} viewDistance to <dark_green><i>$viewDistance")
-                if (duration< Duration.INFINITE) {
-                    delay(duration)
-                    players.forEach { it.viewDistance = it.world.viewDistance }
-                    sender.success("Reset viewDistance for ${if (players.size == 1) players.first().name else players.size.toString() + "players"}!")
+                if (duration < Duration.INFINITE) {
+                    extraCommands.plugin.launch {
+                        delay(duration)
+                        players.forEach { it.viewDistance = it.world.viewDistance }
+                        sender.success("Reset viewDistance for ${if (players.size == 1) players.first().name else players.size.toString() + "players"}!")
+                    }
                 }
             }
+
         }
     }
 
@@ -39,15 +53,28 @@ fun RootIdoCommands.viewDistanceCommands() {
             IntegerArgumentType.integer(0, 32).named("simulationDistance").default { 0 },
             DurationTypeArgument(1.seconds).named("duration").default { Duration.INFINITE }
         ) { players, simulationDistance, duration ->
-            extraCommands.plugin.launch {
+            if (simulationDistance == 0 && duration == Duration.INFINITE) {
+                val simulationDistances = players.groupBy { it.simulationDistance }.entries.sortedBy { it.value.size }
+                if (simulationDistances.size == 1) {
+                    sender.success("SimulationDistances for all players: ${simulationDistances.first().key}")
+                } else sender.success(buildString {
+                    append("SimulationDistances for ${simulationDistances.size} players:\n")
+                    simulationDistances.forEach { (vd, players) ->
+                        if (players.size > 5) append("${players.take(5).joinToString { it.name }},...: $vd\n")
+                        else append("${players.joinToString { it.name }}: $vd\n")
+                    }
+                })
+            } else {
                 players.forEach {
                     it.simulationDistance = simulationDistance.takeIf { it >= 2 } ?: it.world.simulationDistance
                 }
                 sender.success("Successfully set ${if (players.size == 1) "<dark_green>${players.first().name}</dark_green>" else players.size.toString() + "players"} simulationDistance to $simulationDistance")
                 if (duration < Duration.INFINITE) {
-                    delay(duration)
-                    players.forEach { it.simulationDistance = it.world.simulationDistance }
-                    sender.success("Reset simulationDistance for ${if (players.size == 1) players.first().name else players.size.toString() + "players"}!")
+                    extraCommands.plugin.launch {
+                        delay(duration)
+                        players.forEach { it.simulationDistance = it.world.simulationDistance }
+                        sender.success("Reset simulationDistance for ${if (players.size == 1) players.first().name else players.size.toString() + "players"}!")
+                    }
                 }
             }
         }
